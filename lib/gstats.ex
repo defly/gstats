ead304931a98113430fa26ddbbe4f473df808581
Artem Bei <artem.bei@ua.glomex.com>
2017-04-11 16:33:08 +0300

    setup mix project

---
+++ lib/gstats.ex
@@ -0,0 +1,18 @@
+defmodule Gstats do
+  @moduledoc """
+  Documentation for Gstats.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gstats.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gstats_test.exs
@@ -0,0 +1,8 @@
+defmodule GstatsTest do
+  use ExUnit.Case
+  doctest Gstats
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
