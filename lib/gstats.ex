6670d6f7254a96a601b6d31246b7739c39ec0387
defly <defly.self@gmail.com>
2017-04-12 00:11:37 +0300

    updating stats with pulls, need double check

--- lib/gstats.ex
+++ lib/gstats.ex
@@ -18,6 +18,7 @@ end
 
 defmodule Gstats do
   @root_link "https://api.github.com"
+  @user_agent_headers ["User-Agent": "Gstats"]
 
   def repo_link owner, repo do
     ~s{#{@root_link}/repos/#{owner}/#{repo}}
@@ -25,6 +26,10 @@ defmodule Gstats do
 
   def pulls_link owner, repo do
     ~s{#{@root_link}/repos/#{owner}/#{repo}/pulls}
+  end
+
+  def issues_link owner, repo do
+    ~s{#{@root_link}/repos/#{owner}/#{repo}/issues}
   end
 
   def fetch_repo(owner, repo) do
@@ -39,11 +44,7 @@ defmodule Gstats do
     response.body |> Poison.decode! |> length
   end
 
-  def fetch_pulls(owner, repo, state \\ "open") do
-    link = pulls_link(owner, repo)
-    headers = ["User-Agent": "Gstats"]
-    query = %{state: state}
-    response = HTTPotion.get(link, [query: query, headers: headers])
+  defp get_count(response) do
     count_on_page = body_length(response)
     link_header = response.headers["link"]
 
@@ -52,14 +53,35 @@ defmodule Gstats do
     else
       {:ok, links} = ExLinkHeader.parse(link_header)
       last_page = String.to_integer(links.last.params.page)
-      last_page_response = HTTPotion.get(links.last.url, [headers: headers])
+      last_page_response = HTTPotion.get(links.last.url, [headers: @user_agent_headers])
       count_on_page * (last_page - 1) + body_length(last_page_response)
     end
+  end
+
+  def fetch_pulls_counters(owner, repo, state \\ "open") do
+    link = pulls_link(owner, repo)
+    query = %{state: state}
+    HTTPotion.get(link, [query: query, headers: @user_agent_headers])
+      |> get_count
+  end
+
+  def fetch_issues_counters(owner, repo, state \\ "open") do
+    link = issues_link(owner, repo)
+    query = %{state: state}
+    HTTPotion.get(link, [query: query, headers: @user_agent_headers])
+      |> get_count
   end
 
   def stats(owner, repo) do
     contents = fetch_repo(owner, repo);
     atomized = for {key, val} <- contents, into: %{}, do: {String.to_atom(key), val}
-    struct(Gstats.Repo, atomized)
+    repo_stats = struct(Gstats.Repo, atomized)
+    open_pulls = fetch_pulls_counters(owner, repo)
+    closed_pulls = fetch_pulls_counters(owner, repo, "closed")
+    %{repo_stats |
+      open_pulls: open_pulls,
+      closed_pulls: closed_pulls,
+      pulls: open_pulls + closed_pulls
+    }
   end
 end
